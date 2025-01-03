27d60d4cd7cae4f7dda967b855b89384dc6bc376
Nikita Vologdins <nvologdins@gmail.com>
2025-01-03 11:17:15 +0200

    added aria attribute

--- src/modules/Features.tsx
+++ src/modules/Features.tsx
@@ -5,7 +5,9 @@ export default function Features() {
   return (
     <section className="features section">
       <header className="section__header">
-        <h2 className="section__title">Features</h2>
+        <h2 className="section__title" id="tablist-label">
+          Features
+        </h2>
         <p className="section__description">
           Our aim is to make it quick and easy for you to access your favorite
           websites. Your bookmarks sync between your devices so you can access
